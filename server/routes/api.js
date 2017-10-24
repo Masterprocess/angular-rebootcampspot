e3e02c135bffdde43ce2a1aeef7ac844d2d857fb
Christopher Rackley <masterprocess@The-Beluga.local>
2017-10-24 10:28:18 -0400

    server works, gonna hook everything up later today.

---
+++ server/routes/api.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const User = require('../models/video');
+
+const db = "mongodb://rebootcampspotad:j0bsecuritymakesme_sad@ds229415.mlab.com:29415/rebootcampspotdb";
+mongoose.Promise = global.Promise;
+mongoose.connect(db, {
+	useMongoClient: true
+	},
+	function(err){
+	if(err){
+			console.error("Error! " + err);
+	}
+});
+
+router.get('/', function(req, res){
+	res.send('api works');
+});
+
+router.get('/users', function(req,res){
+	console.log("Get request for all users. For testing purposes.");
+	User.find({})
+	.exec(function(err, users){
+		if (err){
+			console.log("Error retrieving users.");
+		}else{
+			res.json(users);
+		}
+	});
+});
+
+router.get('/users/:email', function(req,res){
+	console.log("Get request for a single user. For testing purposes.");
+	User.findone({userEmail: req.params.email})
+	.exec(function(err, user){
+		if (err){
+			console.log("Error retrieving user.");
+		}else{
+			res.json(user);
+		}
+	});
+});
+
+router.post('/user', function(res,req){
+		console.log('Post a new user.');
+		var newUser = new User();
+		newUser.userEmail = req.body.userEmail;
+		newUser.userType = req.body.userType;
+});
+
+module.exports = router;
