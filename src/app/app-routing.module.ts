cdc863f5a2db385151d2e57889d3f230b925f114
Christopher Rackley <masterprocess@The-Beluga.local>
2017-10-24 12:34:37 -0400

    added materialize

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminCenterComponent } from './admin-center/admin-center.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{path: '', redirectTo:'/login', pathMatch:'full'},
+	{path: 'login', component: LoginComponent},
+	{path: 'admin', component: AdminCenterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterializeModule } from 'angular2-materialize';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AdminCenterComponent } from './admin-center/admin-center.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+		AdminCenterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+		AppRoutingModule,
+		MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
