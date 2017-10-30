b9163d9dfa45c2d14c73373d7d1aab4a4f0599ff
Christopher Rackley <masterprocess@The-Beluga.local>
2017-10-30 16:41:05 -0400

    more db work

--- src/app/student-center/student-center.component.ts
+++ src/app/student-center/student-center.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Class } from "../class";
+import { UserService } from '../user.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-student-center',
   templateUrl: './student-center.component.html',
-  styleUrls: ['./student-center.component.css']
+	styleUrls: ['./student-center.component.css'],
+	providers: [UserService]
 })
 export class StudentCenterComponent implements OnInit {
 
-  constructor() { }
+	users: Array<User>;
+
+	selectedUser: User;
+
+  constructor(private _userService: UserService) { }
 
   ngOnInit() {
+		this._userService.getUsers()
+			.subscribe(resUserData => this.users = resUserData);
   }
 
 }
